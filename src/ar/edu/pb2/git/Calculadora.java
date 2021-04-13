7f09a67670b46d6c37d567643e95f0a6875ba960
mat-villalba <matvillalba@alumno.unlam.edu.ar>
2021-04-13 15:03:59 -0300

    cambié nombre a la clase y el if-else por switch

--- src/ar/edu/pb2/git/Calculadora.java
+++ src/ar/edu/pb2/git/Calculadora.java
@@ -1,5 +1,34 @@
 package ar.edu.pb2.git;
 
-public class Calculadora {
+import java.util.Scanner;
 
+public class Calculadora {
+ public static void main(String[] args) {
+	 
+	 Integer a;
+	 Integer b;
+	 Integer opcion;
+	 Integer resultado;
+	 
+	 do{
+		 System.out.println("Ingrese su operación /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
+		 opcion = teclado.next();
+	 }while(opcion < 1 && opcion > 4)
+		 System.out.println("Ingrese el primer numero");
+	 a = teclado.next();
+	 System.out.println("Ingrese el segundo numero");
+	 a = teclado.next();
+	 switch(a) {
+	 	case 1: resultado = a + b;
+	 	break;
+	 	case 2: resultado = a - b;
+	 	break;
+	 	case 3: resultado = a * b;
+	 	break;
+	 	case 4: resultado = a / b;
+	 	break;
+	 	default: System.out.println("Error");
+	 }
+	 System.out.println("El resultado es " + resultado.toString());
+ }
 }
